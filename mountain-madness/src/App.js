fdc8d68dfaa4c08bf522e5f9886a5ae19d8d78aa
Rohan Parmar <rohanxparmar@gmail.com>
2022-04-05 23:27:24 -0700

    Basic Layout

---
+++ mountain-madness/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "style.css"
+import "tailwindcss/dist/base.css"
+import AnimationRevealPage from "helpers/AnimationRevealPage"
+import Hero from "components/hero/FullWidthWithImage"
+
+function App() {
+  return (
+    <AnimationRevealPage>
+      <Hero />
+    </AnimationRevealPage>
+  )
+}
+
+export default App
